fc33974599af4df5b321ccd13ee48a437c082b45
Adam Meily <meily.adam@gmail.com>
2014-06-10 23:57:43 -0400

    added comment and citation routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 
   root 'site#index'
 
-  resources :souls
+  resources :souls do
+    resources :citations
+    resources :comments
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
