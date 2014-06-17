eb246fed0790b41c953a675082c418a779bca651
Adam Meily <meily.adam@gmail.com>
2014-06-16 23:14:51 -0400

    added attachments migrate

---
+++ db/migrate/20140613031252_create_attachments.rb
@@ -0,0 +1,26 @@
+class CreateAttachments < ActiveRecord::Migration
+  def change
+    create_table :attachments do |t|
+      t.string :customer
+      t.string :name
+      t.text :description
+      t.string :category
+      t.references :soul, index: true
+      t.string :attachment_file_name
+      t.string :attachment_content_type
+      t.integer :attachment_file_size
+      t.timestamps
+    end
+  end
+
+  #def self.up
+  #  add_column :attachments, :attachment_file_name, :string
+  #  add_column :attachments, :attachment_content_type, :string
+  #  add_column :attachments, :attachment_file_size, :integer
+  #end
+  #def self.down
+  #  remove_column :attachments, :attachment_file_name
+  #  remove_column :attachments, :attachment_content_type
+  #  remove_column :attachments, :attachment_file_size
+  #end
+end
