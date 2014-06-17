e8936b23451a460fed0b9226803ae7af3cea01f6
Adam Meily <meily.adam@gmail.com>
2014-06-16 23:13:33 -0400

    added citation validation

--- app/controllers/citations_controller.rb
+++ app/controllers/citations_controller.rb
@@ -2,10 +2,14 @@ class CitationsController < ApplicationController
     def create
         @soul = Soul.find(params[:soul_id])
         #redirect_to soul_path(@soul)
-        @citation = @soul.citations.create(citation_params)
+        @citation = @soul.citations.new(citation_params)
         
         respond_to do |format|
-            format.json { render json: @citation, status: :created }
+            if @citation.save
+                format.json { render json: @citation, status: :created }
+            else
+                format.json { render json: @citations.errors.messages, status: :bad_request }
+            end
         end
     end
 
--- app/models/citation.rb
+++ app/models/citation.rb
@@ -1,3 +1,4 @@
 class Citation < ActiveRecord::Base
   belongs_to :soul
+  validates :url, presence: true, :format => URI::regexp(%w(http https))
 end
