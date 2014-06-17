bc151ae0d458dc488801fcffef1447c6332b84f0
Adam Meily <meily.adam@gmail.com>
2014-06-16 23:13:46 -0400

    removed soul_links and added attachments

--- app/models/soul.rb
+++ app/models/soul.rb
@@ -2,5 +2,5 @@ class Soul < ActiveRecord::Base
     validates :name, :description, presence: true
     has_many :comments
     has_many :citations
-    has_many :soul_links
+    has_many :attachments
 end
